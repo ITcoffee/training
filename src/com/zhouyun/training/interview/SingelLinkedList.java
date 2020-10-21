5a0d2a1829c36e98c62571738add525ccf9c4099
ZY <ZY@DESKTOP-4UMHT1U>
2020-10-21 16:46:57 +0800

    单链表

--- src/com/zhouyun/training/interview/SingelLinkedList.java
+++ src/com/zhouyun/training/interview/SingelLinkedList.java
@@ -2,7 +2,7 @@ package com.zhouyun.training.interview;
 
 import java.util.List;
 
-/** 
+/** 	
  * 单链表，已知头结点，删除倒数第n个节点，并返回头结点
  *
  * @author  周云
@@ -13,17 +13,17 @@ public class SingelLinkedList {
 	public Node delete(Node root, List<Node> nodes, int n) {
 		if (n > nodes.size()) {
 			System.out.println("超出最大节点数！");
-			return null;
+			return root;
 		}
 		
 		if (n == nodes.size()) {
-			return root.next;
+			return root.getNext();
 		}
 		
 		Node deleteNode = root;
 		
 		for (int i = 0; i < nodes.size() - n; i++) {
-			deleteNode = deleteNode.next;
+			deleteNode = deleteNode.getNext();
 		}
 		
 		nodes.remove(deleteNode);
@@ -36,19 +36,23 @@ public class SingelLinkedList {
 		private int value;
 		private Node next;
 		
-		public Node(int value) {
+		public Node(int value, Node next) {
 			this.value = value;
+			this.next = next;
 		}
 		
 		public int getValue() {
 			return value;
 		}
+		
 		public void setValue(int value) {
 			this.value = value;
 		}
+		
 		public Node getNext() {
 			return next;
 		}
+		
 		public void setNext(Node next) {
 			this.next = next;
 		}
